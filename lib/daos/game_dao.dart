a6c050889305991c054f784cde5781a06fe0b3b1
Hugo <hugo.reyhol@educa.jcyl.es>
2025-01-11 20:31:43 +0100

    Añadido DAO para Game

---
+++ lib/daos/game_dao.dart
@@ -0,0 +1,38 @@
+import 'package:gestion_juegos/models/game.dart';
+import 'package:gestion_juegos/util/db_manager.dart';
+
+class GameDao {
+  static Future<Game?> getGameById(int idGame) async {
+    final db = await DbManager().database;
+
+    final result = await db.query("Games", where: "idGame = ?", whereArgs: [idGame]);
+
+    return result.isEmpty ? null : Game.fromMap(result.first);
+  }
+
+  static Future<List<Game>> getGamesByTitle(String title) async {
+    final db = await DbManager().database;
+
+    final result = await db.query("Games", where: "title LIKE ?", whereArgs: ["%$title%"]);
+
+    final List<Game> games = [];
+    for (var game in result) {
+      games.add(Game.fromMap(game));
+    }
+
+    return games;
+  }
+
+  static Future<List<Game>> getGames() async {
+    final db = await DbManager().database;
+
+    final result = await db.query("Games");
+
+    final List<Game> games = [];
+    for (var game in result) {
+      games.add(Game.fromMap(game));
+    }
+
+    return games;
+  }
+}
