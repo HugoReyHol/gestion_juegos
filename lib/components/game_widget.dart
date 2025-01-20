ecd6663d25f7714b5c765391294b737430b551b0
Hugo <hugo.reyhol@educa.jcyl.es>
2025-01-20 22:48:38 +0100

    Game Widget recibe game en vez de usergame

--- lib/components/game_widget.dart
+++ lib/components/game_widget.dart
@@ -4,52 +4,33 @@ import 'package:gestion_juegos/models/user_game.dart';
 import 'package:gestion_juegos/models/game.dart';
 
 class GameWidget extends StatefulWidget {
-  final UserGame userGame;
+  final Game _game;
 
-  const GameWidget({super.key, required this.userGame});
+  const GameWidget({super.key, required Game game}) : _game = game;
 
   @override
   State<GameWidget> createState() => _GameWidgetState();
 }
 
 class _GameWidgetState extends State<GameWidget> {
-  late final Game game;
-  bool _loading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadGame();
-  }
-
-  void _loadGame() async{
-    game = (await GameDao.getGameById(widget.userGame.idGame))!;
-
-    setState(() {
-      _loading = false;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return _loading ?
-      Text("Cargando") :
-      GestureDetector(
-        onTap: () => Navigator.pushNamed(context, "/details", arguments: {"userGame": widget.userGame}),
-        child: Card(
-          clipBehavior: Clip.antiAlias,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.memory(game.image),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: Text(game.title, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold), maxLines: 2, overflow: TextOverflow.ellipsis, )
-              )
-            ],
-          ),
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, "/details", arguments: {"game": widget._game}),
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.memory(widget._game.image),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(widget._game.title, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold), maxLines: 2, overflow: TextOverflow.ellipsis, )
+            )
+          ],
         ),
-      );
+      ),
+    );
   }
 }
