fd2dcd38b43e277b4ef4f735834bb3e45c4c91e7
Hugo <hugo.reyhol@educa.jcyl.es>
2025-01-21 09:53:20 +0100

    Cambiado orden lista notas

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -17,7 +17,7 @@ class _DetailsState extends State<Details> {
   Game? _game;
   UserGame? _userGame;
   bool _loading = true;
-  final List<String> _scoreValues = ["Sin seleccionar", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"].reversed.toList();
+  final List<String> _scoreValues = ["10", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", "Sin seleccionar"];
   final TextEditingController _timePlayedCtrll = TextEditingController();
 
 
