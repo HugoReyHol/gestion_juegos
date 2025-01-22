3e3f6f1a381dc1f448aebea8f8fb36dfad80cfe6
Hugo <hugo.reyhol@educa.jcyl.es>
2025-01-22 13:44:29 +0100

    Agregados metodos a games_provider

--- lib/providers/games_provider.dart
+++ lib/providers/games_provider.dart
@@ -1,10 +1,24 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gestion_juegos/daos/game_dao.dart';
 import 'package:gestion_juegos/models/game.dart';
 
 class GamesNotifier extends StateNotifier<List<Game>> {
+  Game? _currentGame;
   GamesNotifier() : super([]);
 
+  void getGames() async {
+    state = await GameDao.getGames();
+  }
 
+  void getGamesByTitle(String title) async {
+    state = await GameDao.getGamesByTitle(title);
+  }
+
+  Game? getCurrentGame() => _currentGame;
+
+  set currentGame(Game value) {
+    _currentGame = value;
+  }
 }
 
 final gamesProvider = StateNotifierProvider<GamesNotifier, List<Game>>((ref) => GamesNotifier());
