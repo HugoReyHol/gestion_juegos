91b34808d50a73cb7b1485fc7f0e079f357a49dd
Hugo <hugo.reyhol@educa.jcyl.es>
2025-01-23 11:35:19 +0100

    Guarda el juego actual en el provider games en vez de pasarlo con Navegator

--- lib/components/game_widget.dart
+++ lib/components/game_widget.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gestion_juegos/models/game.dart';
+import 'package:gestion_juegos/providers/games_provider.dart';
 
 enum LayoutMode {vertical, horizontal}
 
-class GameWidget extends StatelessWidget {
+class GameWidget extends ConsumerWidget {
   final Game _game;
   final LayoutMode layoutMode;
 
   const GameWidget({super.key, required Game game, required this.layoutMode}) : _game = game;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     switch (layoutMode) {
-      case LayoutMode.vertical: return _buildVertical(context);
-      case LayoutMode.horizontal: return _buildHorizontal(context);
+      case LayoutMode.vertical: return _buildVertical(context, ref);
+      case LayoutMode.horizontal: return _buildHorizontal(context, ref);
     }
   }
 
-  Widget _buildVertical(BuildContext context) {
+  Widget _buildVertical(BuildContext context, WidgetRef ref) {
     return GestureDetector(
-      onTap: () => Navigator.pushNamed(context, "/details", arguments: {"game": _game}),
+      onTap: () {
+        ref.read(gamesProvider.notifier).currentGame = _game;
+        Navigator.pushNamed(context, "/details");
+      },
       child: Card(
         clipBehavior: Clip.antiAlias,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
@@ -42,12 +47,15 @@ class GameWidget extends StatelessWidget {
 
   // TODO mejorar el display horizontal
   // TODO si el usuario ya tiene el juego no mostrar el boton
-  Widget _buildHorizontal(BuildContext context) {
+  Widget _buildHorizontal(BuildContext context, WidgetRef ref) {
     return Stack(
       alignment: Alignment.bottomRight,
       children: [
         GestureDetector(
-          onTap: () => Navigator.pushNamed(context, "/details", arguments: {"game": _game}),
+          onTap: () {
+            ref.read(gamesProvider.notifier).currentGame = _game;
+            Navigator.pushNamed(context, "/details");
+          },
           child: Card(
             clipBehavior: Clip.antiAlias,
             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
