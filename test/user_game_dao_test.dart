a2c1680833d44a9e1338b25f560996cf35ef05ec
Hugo <hugo.reyhol@educa.jcyl.es>
2025-01-24 13:37:59 +0100

    Cambiando dao

---
+++ test/user_game_dao_test.dart
@@ -0,0 +1,46 @@
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gestion_juegos/daos/user_game_dao.dart';
+import 'package:gestion_juegos/models/user_game.dart';
+import 'package:gestion_juegos/util/db_manager.dart';
+
+void main() {
+  test("Insertar juegos a usuario", () async {
+    await UserGameDao.insertUserGame(UserGame(idGame: 1, idUser: 1));
+    await UserGameDao.insertUserGame(UserGame(idGame: 2, idUser: 1));
+  });
+
+  test("Leer juegos a usuario", () async {
+    List<UserGame> games = await UserGameDao.getUserGames(1);
+
+    print(games.length);
+
+    for (UserGame game in games) {
+      print(game.toMap());
+    }
+  });
+
+  test("Eliminar juegos de usuairo", () async {
+    await UserGameDao.deleteUserGame(UserGame(idGame: 1, idUser: 1));
+    await UserGameDao.deleteUserGame(UserGame(idGame: 1, idUser: 2));
+
+  });
+
+  test("Obtener el userGame con su game", () async {
+    final db = await DbManager().database;
+
+    // print(await db.rawQuery("""
+    //   SELECT Users_Games.*, Games.*
+    //   FROM Users_Games, Games
+    //   WHERE Users_Games.idUser = 1 AND Users_Games.idGame = Games.idGame
+    // """));
+
+    print(await db.rawQuery("""
+      SELECT Users_Games.*, Games.*
+      FROM Users_Games      
+      JOIN Games ON Users_Games.idGame = Games.idGame
+      WHERE Users_Games.idUser = 1
+    """));
+  });
+
+}
