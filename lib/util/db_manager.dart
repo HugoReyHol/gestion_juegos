d7bc353df5ad789372a1739109bd7d63aaaa391f
Hugo <hugo.reyhol@educa.jcyl.es>
2025-01-25 13:50:40 +0100

    Añadidos más juegos a la db

--- lib/util/db_manager.dart
+++ lib/util/db_manager.dart
@@ -86,8 +86,58 @@ class DbManager {
           "Mac: 2010-5-12\n"
           "Linux: 2013-5-2\n"
           "Android: 2014-5-12\n"
-          "Nintendo Switch: 2022-6-28\n"
-      )
+          "Nintendo Switch: 2022-6-28"
+      ),
+      Game(idGame: 3,
+        title: "The Legend of Zelda: Majora's Mask 3D",
+        description: "The Legend of Zelda: Majora's Mask 3D is a remake of the original Nintendo 64 game with more up-to-date graphics, streamlined UI and different additional game modes. Most textures are significantly more detailed, and many models are more faithful to the game's concept and promotional art. In addition, the frame rate has been increased to 30 FPS compared to the original's 20 FPS.",
+        image: await File("assets/zelda_mm.png").readAsBytes(),
+        details: "Genres: Role-playing (RPG), Adventure, Action, Fantasy\n"
+          "Game mode: Single player\n"
+          "Developer: Grezzo\n"
+          "Publisher: Nintendo",
+        releases: "Nintendo 3DS: EU 2015-2-13"
+      ),
+      Game(idGame: 4,
+          title: "BioShock",
+          description: "BioShock is a horror-themed first-person shooter set in a steampunk underwater dystopia. The player is urged to turn everything into a weapon: biologically modifying their own body with Plasmids, hacking devices and systems, upgrading their weapons, crafting new ammo variants, and experimenting with different battle techniques are all possible.",
+          image: await File("assets/bioshock.png").readAsBytes(),
+          details: "Genres: Shooter, Puzzle, Role-playing (RPG), Adventure, Action, Sci-Fi, Horror, Stealth\n"
+              "Game mode: Single player\n"
+              "Developer: 2K Boston, 2K Australia\n"
+              "Publisher: 2K Games",
+          releases: "Windows: 2007-8-21\n"
+              "Xbox 360: EU 2007-8-24\n"
+              "Mac: 2009-10-23"
+      ),
+      Game(idGame: 5,
+          title: "Helldivers 2",
+          description: "The Galaxy’s Last Line of Offence. Enlist in the Helldivers and join the fight for freedom across a hostile galaxy in a fast, frantic, and ferocious third-person shooter.",
+          image: await File("assets/helldivers_2.png").readAsBytes(),
+          details: "Genres: Shooter, Tactical, Action, Sci-Fi\n"
+              "Game mode: Single player, Multiplayer, Co-operative\n"
+              "Developer: Arrowhead Game Studios\n"
+              "Publisher: Sony Interactive Entertainment",
+          releases: "Windows: 2024-2-8\n"
+              "PlayStation 5: 2024-2-8"
+      ),
+      Game(idGame: 6,
+          title: "Inscryption",
+          description: "Inscryption is an inky black card-based odyssey that blends the deckbuilding roguelike, escape-room style puzzles, and psychological horror into a blood-laced smoothie. Darker still are the secrets inscrybed upon the cards...",
+          image: await File("assets/inscryption.png").readAsBytes(),
+          details: "Genres: Puzzle, Strategy, Adventure, Indie, Card & Board Game, Horror, Mystery\n"
+              "Game mode: Single player\n"
+              "Developer: Daniel Mullins Games\n"
+              "Publisher: Devolver Digital, Daniel Mullins Games",
+          releases: "Windows: 2021-10-19\n"
+              "Xbox Series X|S: 2023-4-10\n"
+              "Xbox One: 2023-4-10\n"
+              "PlayStation 5: 2022-8-30\n"
+              "PlayStation 4: 2022-8-30\n"
+              "Mac: 2021-10-19\n"
+              "Linux: 2021-10-19\n"
+              "Nintendo Switch: 2022-12-1"
+      ),
     ];
 
     // TODO comprobar si se puede hacer con DAOs
