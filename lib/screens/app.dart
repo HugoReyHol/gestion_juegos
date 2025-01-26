954063aed90a5966e3eb6510a0c3cadadee9d4df
Hugo <hugo.reyhol@educa.jcyl.es>
2025-01-26 21:58:55 +0100

    Empezado menú lateral

--- lib/screens/app.dart
+++ lib/screens/app.dart
@@ -14,14 +14,18 @@ class App extends ConsumerStatefulWidget {
 
 class _AppState extends ConsumerState<App> {
   final List<Widget> _screens = [Home(), Search(), Stats()];
-  int _actualScren = 0;
+  int _actualScreen = 0;
 
+  // TODO hacer que al pasar de ancho se abra ventana lateral en vez del NavigationBar
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+      ),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
-        child: _screens[_actualScren]
+        padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+        child: _screens[_actualScreen]
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: <BottomNavigationBarItem>[
@@ -29,15 +33,48 @@ class _AppState extends ConsumerState<App> {
           BottomNavigationBarItem(icon: Icon(Icons.search_outlined), activeIcon: Icon(Icons.search), label: "Buscar"),
           BottomNavigationBarItem(icon: Icon(Icons.table_chart_outlined), activeIcon: Icon(Icons.table_chart), label: "Estadísticas"),
         ],
-        currentIndex: _actualScren,
+        currentIndex: _actualScreen,
         onTap: (value) {
-          setState(() {
-            ref.read(gamesProvider.notifier).resetGames();
-            _actualScren = value;
-          });
+          changeScreen(value);
         },
+      ),
+      drawer: Drawer(
+        width: 250,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            ListTile(
+              title: Text("Ventanas"),
+            ),
+            Divider(),
+            ListTile(
+              selected: _actualScreen == 0,
+              leading: Icon(Icons.home),
+              title: Text("Inicio"),
+              onTap: () => changeScreen(0),
+            ),
+            ListTile(
+              selected: _actualScreen == 1,
+              leading: Icon(Icons.search),
+              title: Text("Buscar"),
+              onTap: () => changeScreen(1),
+            ),
+            ListTile(
+              selected: _actualScreen == 2,
+              leading: Icon(Icons.table_chart),
+              title: Text("Estadísticas"),
+              onTap: () => changeScreen(2),
+            ),
+          ],
+        ),
       ),
     );
   }
-}
 
+  void changeScreen(int screen) {
+    setState(() {
+      ref.read(gamesProvider.notifier).resetGames();
+      _actualScreen = screen;
+    });
+  }
+}
