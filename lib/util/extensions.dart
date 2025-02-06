3af5b90c82a252fe02520050bd078d1c44ce90e1
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-06 20:11:33 +0100

    Creada extension para localizar estados

--- lib/util/extensions.dart
+++ lib/util/extensions.dart
@@ -1,5 +1,8 @@
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
+import 'package:flutter/material.dart';
+import 'package:gestion_juegos/models/user_game.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 extension StringExtensions on String {
 
@@ -7,4 +10,18 @@ extension StringExtensions on String {
 
   String capitalize() => this[0].toUpperCase() + substring(1);
 
+}
+
+extension GameStatesExtensions on GameStates {
+  String localize(BuildContext context) {
+    final loc = AppLocalizations.of(context)!;
+
+    switch (this) {
+      case GameStates.playing: return loc.state_playing;
+      case GameStates.completed: return loc.state_completed;
+      case GameStates.on_hold: return loc.state_hold;
+      case GameStates.dropped: return loc.state_dropped;
+      case GameStates.plan_to_play: return loc.state_plan;
+    }
+  }
 }
