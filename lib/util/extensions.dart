74107d8e0e1a6a907c96b9e8e9589ee0a928209c
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-08 10:32:50 +0100

    extensions comentado

--- lib/util/extensions.dart
+++ lib/util/extensions.dart
@@ -4,15 +4,26 @@ import 'package:flutter/material.dart';
 import 'package:gestion_juegos/models/user_game.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+/// Extensiones para `String`
 extension StringExtensions on String {
 
+  /// Encripta un `String`
+  ///
+  /// Devuelve un `String` encriptado en sha256
   String encrypt() => sha256.convert(utf8.encode(this)).toString();
 
-  String capitalize() => this[0].toUpperCase() + substring(1);
+  /// Capitaliza un `String`
+  ///
+  /// Devuelve un `String` con la primera letra en mayúscula y el resto en
+  /// minúsculas
+  String capitalize() => this[0].toUpperCase() + substring(1).toLowerCase();
 
 }
 
+/// Extensiones para `GameStates`
 extension GameStatesExtensions on GameStates {
+
+  /// Localiza los valores de `GameStates`
   String localize(BuildContext context) {
     final loc = AppLocalizations.of(context)!;
 
