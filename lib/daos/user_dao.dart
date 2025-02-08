5d50132bab5b44d47b76f51a4a2528c28870b770
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-08 11:18:25 +0100

    user_dao comentado

--- lib/daos/user_dao.dart
+++ lib/daos/user_dao.dart
@@ -1,7 +1,11 @@
 import 'package:gestion_juegos/util/db_manager.dart';
 import '../models/user.dart';
 
+/// Clase para interactuar con los usuario de la base de datos
 abstract class UserDao {
+  /// Obtiene un usuario a partir de su nombre
+  ///
+  /// Devuelve un `User` si existe y `null` si no lo hace
   static Future<User?> getUser(String name) async {
     final db = await DbManager.database;
 
@@ -10,6 +14,9 @@ abstract class UserDao {
     return result.isEmpty ? null : User.fromMap(result.first);
   }
 
+  /// Inserta un usuario en la base de datos
+  ///
+  /// Devuelve un `int` con el valor del id creado
   static Future<int> insertUser(User user) async {
     final db = await DbManager.database;
 
