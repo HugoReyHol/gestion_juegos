fdc6775cf54cb2cf5e8f8ec1a700c6a6db54e005
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-08 11:19:20 +0100

    game_dao comentado

--- lib/daos/game_dao.dart
+++ lib/daos/game_dao.dart
@@ -1,7 +1,11 @@
 import 'package:gestion_juegos/models/game.dart';
 import 'package:gestion_juegos/util/db_manager.dart';
 
+/// Clase para interactuar con los juegos de la base de datos
 abstract class GameDao {
+  /// Obtiene todos los juegos de la base de datos
+  ///
+  /// Devuelve una `List<Game>` con los juegos
   static Future<List<Game>> getGames() async {
     final db = await DbManager.database;
 
