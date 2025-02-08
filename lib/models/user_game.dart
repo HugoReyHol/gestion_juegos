f82dc749f1e521c4e89e26cf8ed30d8f34824c2b
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-08 10:59:08 +0100

    user_game comentado

--- lib/models/user_game.dart
+++ lib/models/user_game.dart
@@ -1,11 +1,19 @@
+/// Representa el juego de un usuario de la base de datos
 class UserGame {
+  /// Id del juego al que añade información
   late int idGame;
+  /// Id del usuario al que pertenece
   late int idUser;
+  /// Nota que ha puesto el usuario
   int? score;
+  /// Tiempo jugado por el usuario
   int timePlayed;
+  /// Estado en el que se encuentra el juego
   GameStates gameState;
+  /// La última vez que se actualizó el juego
   DateTime lastChange;
 
+  /// Constructor base
   UserGame({
     required this.idGame,
     required this.idUser,
@@ -15,6 +23,7 @@ class UserGame {
     required this.lastChange
   });
 
+  /// Devuelve una copia del juego de usuario
   UserGame copyWith() {
     return UserGame(
       idGame: idGame,
@@ -26,6 +35,9 @@ class UserGame {
     );
   }
 
+  /// Constructor de juegos de usuario a partir de un mapa
+  ///
+  /// Necesarío para crear la clase `UserGame` a partir de los datos de la base de datos
   factory UserGame.fromMap(Map<String, dynamic> map) {
     return UserGame(
       idGame: map["idGame"],
@@ -37,6 +49,10 @@ class UserGame {
     );
   }
 
+  /// Crea un mapa a partir de los datos de `UserGame`
+  ///
+  /// Necesario para inserta el juego en la base de datos
+  /// Devuelve un `Map<String, dynamic>` con los valores
   Map<String, dynamic> toMap() => {
     "idGame": idGame,
     "idUser": idUser,
@@ -48,4 +64,5 @@ class UserGame {
 
 }
 
+/// Enum con los distintos estados que puede tener un juego de usuario
 enum GameStates {playing, completed, on_hold, dropped, plan_to_play}
