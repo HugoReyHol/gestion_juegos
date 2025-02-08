559539957e216ec956795b144e98f4f2c306eb6c
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-08 09:55:55 +0100

    user_provider comentado

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -3,10 +3,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gestion_juegos/models/user.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// El notifier del usuario actual
+///
+/// El usuario por defecto es null y al iniciar la app busca si hay un usuario
+/// cargado en las shared preferences
 class UserNotifier extends Notifier<User?> {
   @override
   User? build() => null;
 
+  /// Obtiene el usuario de las shared preferences
+  ///
+  /// Devuelve `true` si lo ha encontrado y `false` en caso contrario
   Future<bool> getSavedUser() async {
     final prefs = await SharedPreferences.getInstance();
 
@@ -26,6 +33,7 @@ class UserNotifier extends Notifier<User?> {
     return true;
   }
 
+  /// Guarda el usuario actual en las shared preferences
   void saveUser() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setInt("idUser", state!.idUser!);
@@ -33,6 +41,7 @@ class UserNotifier extends Notifier<User?> {
     await prefs.setString("password", state!.password);
   }
 
+  /// Borra el usuario actual de las shared preferences
   void deleteSavedUser(BuildContext context) async {
     final prefs = await SharedPreferences.getInstance();
 
@@ -45,4 +54,5 @@ class UserNotifier extends Notifier<User?> {
   }
 }
 
+/// El provider del usuario actual
 final userProvider = NotifierProvider<UserNotifier, User?>(() => UserNotifier());
