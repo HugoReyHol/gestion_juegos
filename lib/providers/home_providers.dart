0a09a07152bee60dacc19d625390faad52e3d626
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-08 10:23:11 +0100

    home_providers comentado

--- lib/providers/home_providers.dart
+++ lib/providers/home_providers.dart
@@ -4,8 +4,12 @@ import 'package:gestion_juegos/models/user_game.dart';
 import 'package:gestion_juegos/providers/games_provider.dart';
 import 'package:gestion_juegos/providers/user_games_provider.dart';
 
+/// Provider del estado seleccionado en el filtro de home
+///
+/// Por defecto empieza en `GameStates.playing`
 final stateProvider = StateProvider<GameStates?>((ref) => GameStates.playing);
 
+/// Provider de los juegos filtrados que se ven en home
 final homeGamesProvider = Provider<List<Game>>((ref) {
   final games = ref.watch(gamesProvider);
   final userGames = ref.watch(userGamesProvider);
