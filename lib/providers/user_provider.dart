bb0c4f447ef26a4bec426b6d626e63db979a9319
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-09 13:59:58 +0100

    user_provider usa service

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gestion_juegos/models/user.dart';
+import 'package:gestion_juegos/services/user_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 /// El notifier del usuario actual
@@ -17,26 +18,19 @@ class UserNotifier extends Notifier<User?> {
   Future<bool> getSavedUser() async {
     final prefs = await SharedPreferences.getInstance();
 
-    final idUser = prefs.getInt("idUser");
     final name = prefs.getString("name");
     final password = prefs.getString("password");
 
-    if (idUser == null || name == null || password == null) return false;
+    if (name == null || password == null) return false;
 
-    final User user = User(
-      name: name,
-      password: password
-    );
-    user.idUser = idUser;
+    state = await UserService.getUser(name, password);
 
-    state = user;
     return true;
   }
 
   /// Guarda el usuario actual en las shared preferences
   void saveUser() async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt("idUser", state!.idUser!);
     await prefs.setString("name", state!.name);
     await prefs.setString("password", state!.password);
   }
@@ -45,7 +39,6 @@ class UserNotifier extends Notifier<User?> {
   void deleteSavedUser(BuildContext context) async {
     final prefs = await SharedPreferences.getInstance();
 
-    await prefs.remove("idUser");
     await prefs.remove("name");
     await prefs.remove("password");
     if (context.mounted) {
